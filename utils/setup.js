2dac02b47cac18b6075fa3cda1c93ca81945e2b7
Ajit <me@ajitkumar.dev>
2022-09-06 15:49:44 +0530

    added setup script

---
+++ utils/setup.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+const { exec } = require('child_process');
+
+console.log('Running npm install...');
+exec('npm install', (error) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  console.log('Add android...');
+  exec('npm run clean', (cleanError) => {
+    if (cleanError) {
+      console.error(cleanError);
+      process.exit(1);
+    }
+
+    console.log('Android added.');
+    console.log('Intalling native plugin.');
+    exec('cordova plugin add ./native', (pluginError) => {
+      if (pluginError) {
+        console.error(pluginError);
+        process.exit(1);
+      }
+      console.log('Native plugin installed.');
+    });
+  });
+});
